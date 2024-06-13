5a0ffe42c4f791d01916b6a4552fb141768f0208
Louis.S <louis.sasse@protonmail.com>
2024-06-14 00:08:10 +0200

    fix: scritp ssh-key generate

--- generate_ssh.sh
+++ generate_ssh.sh
@@ -9,11 +9,15 @@ key_path="/root/.ssh/${key_name}"
 # Créer le répertoire .ssh s'il n'existe pas
 mkdir -p /root/.ssh
 
+# Obtenir l'IP du conteneur
+# Supposons que vous utilisez Docker, vous pouvez utiliser la commande suivante pour obtenir l'IP du conteneur :
+container_ip=$(hostname -I | awk '{print $1}')
+
+# Si vous utilisez un autre environnement ou méthode, remplacez la ligne ci-dessus par la méthode appropriée pour obtenir l'IP.
 
 if [ -f "$key_path" ]; then
     echo "La clé privée $key_path existe déjà. Aucune nouvelle clé ne sera générée."
 else
-    # Générer la paire de clés SSH
-    ssh-keygen -t rsa -b 4096 -f "$key_path" -N ""
+    # Générer la paire de clés SSH avec l'IP du conteneur dans le commentaire
+    ssh-keygen -t rsa -b 4096 -f "$key_path" -N "" -C "root@${container_ip}"
 fi
-
